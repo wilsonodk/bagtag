47385e785f15618e26fcba75c9c324a9b11791b8
Wilson Wise <wilson.wise@turner.com>
2016-01-04 15:57:38 -0500

    moving some templates for better support

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,20 +7,22 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template("AppBundle:default:index.html.twig")
      * @Method("GET")
      */
     public function indexAction(Request $request)
     {
         $players = $this->getDoctrine()->getRepository('AppBundle:Player')->findAllOrderedByRank();
 
-        return $this->render('default/index.html.twig', array(
+        return array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
             'players' => $players,
-        ));
+        );
     }
 }
